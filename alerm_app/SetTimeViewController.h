de01fcc4d2e8e999e2a7837c6897bd48cc8930ce
諸原聖 <satoshi@STB1012187.local>
2015-07-04 22:52:16 +0900

    viewを追加し、classを作成した

---
+++ alerm_app/SetTimeViewController.h
@@ -0,0 +1,14 @@
+//
+//  SetTimeViewController.h
+//  alerm_app
+//
+//  Created by 諸原聖 on 2015/07/04.
+//  Copyright (c) 2015年 fun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SetTimeViewController : UIViewController
+- (IBAction)BackButtonAction:(id)sender;
+
+@end
